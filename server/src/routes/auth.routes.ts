9b74303ca02392a3d240a26e80c1a36d8ebb7fa0
k-pilarski <karolpilarski04@gmail.com>
2026-03-24 17:46:28 +0100

    feat: add role-based authorization middleware (RBAC)

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { register, login } from '../controllers/auth.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
+import { authorizeRoles } from '../middlewares/role.middleware.js';
 
 const router = Router();
 
@@ -8,10 +9,15 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.get('/me', protect, (req, res) => {
-  res.json({ 
-    message: 'Masz dostęp do tajnych danych!', 
-    user: (req as any).user 
-  });
+  res.json({ message: 'Masz dostęp do tajnych danych!', user: (req as any).user });
+});
+
+router.get('/instructor-panel', protect, authorizeRoles('INSTRUCTOR', 'ADMIN'), (req, res) => {
+  res.json({ message: 'Witaj w panelu instruktora! Możesz tu dodawać kursy.' });
+});
+
+router.get('/admin-only', protect, authorizeRoles('ADMIN'), (req, res) => {
+  res.json({ message: 'Witaj, Wielki Administratorze.' });
 });
 
 export default router;
