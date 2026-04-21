d48d42d0b6c75db7845bc29185ba1e109381b916
k-pilarski <karolpilarski04@gmail.com>
2026-04-21 18:34:28 +0200

    feat: implement lesson progress API and completion logic (#26, #27)

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth.routes.js';
 import cors from 'cors';
 import courseRoutes from './routes/course.routes.js';
 import uploadRoutes from './routes/upload.routes.js';
+import progressRoutes from './routes/progress.routes.js';
 
 const app: Express = express();
 const PORT = process.env.PORT || 5000;
@@ -20,6 +21,7 @@ app.use(cors({ origin: 'http://localhost:5173' }));
 app.use('/api/auth', authRoutes); 
 app.use('/api/courses', courseRoutes);
 app.use('/api/uploads', uploadRoutes);
+app.use('/api/progress', progressRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Serwer uruchomiony na porcie ${PORT}`);
